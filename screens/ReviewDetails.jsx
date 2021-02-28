c3d180841f8f4956cc957284c3f016580ea9b0f4
sky1o1 <aakash.dangol101@gmail.com>
2021-02-28 15:14:34 +0545

    [add] images

--- screens/ReviewDetails.jsx
+++ screens/ReviewDetails.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import globalStyles from '../styles/global';
+import {StyleSheet,View, Text, Image} from 'react-native';
+import {globalStyles, images} from '../styles/global';
+import Card from '../shared/Card';
 
 function ReviewDetails({navigation}){
-    console.log(navigation.getParam('title'))
+    const rating = navigation.getParam('rating');
 
     return(
         <View style={globalStyles.container}>
-            <Text>{navigation.getParam('title')}</Text>
-            <Text>{navigation.getParam('body')}</Text>
-            <Text>{navigation.getParam('rating')}</Text>
+            <Card>
+                <Text>{navigation.getParam('title')}</Text>
+                <Text>{navigation.getParam('body')}</Text>
+                <View style={styles.rating}>
+                <Text>GameZone Rating:</Text>
+                {/* <Image source={images.ratings[rating]}/> */}
+                <img src={images.ratings[rating]}/>
+                </View>
+            </Card>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    rating: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      paddingTop: 16,
+      marginTop: 16,
+      borderTopWidth: 1,
+      borderTopColor: '#eee',
+    }
+  });
 
 export default ReviewDetails;
